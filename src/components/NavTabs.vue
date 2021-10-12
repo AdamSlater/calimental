139c7a575741d7ad2cbf8dc4a1eb4a704b35c348
AdamSlater <adam.slater.1010@gmail.com>
2021-10-12 16:59:47 -0700

    extracted nav tabs and user details components

---
+++ src/components/NavTabs.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="tabs is-centered is-boxed">
+    <ul style="border-bottom: 1px solid #dbdbdb">
+      <router-link
+        to="/skills/push"
+        v-slot="{ href, navigate, isActive }"
+        custom
+      >
+        <li :class="[isActive && 'is-active']">
+          <a :href="href" @click="navigate">Push</a>
+        </li>
+      </router-link>
+      <router-link
+        to="/skills/pull"
+        v-slot="{ href, navigate, isActive }"
+        custom
+      >
+        <li :class="[isActive && 'is-active']">
+          <a :href="href" @click="navigate">Pull</a>
+        </li>
+      </router-link>
+      <router-link
+        to="/skills/legs"
+        v-slot="{ href, navigate, isActive }"
+        custom
+      >
+        <li :class="[isActive && 'is-active']">
+          <a :href="href" @click="navigate">Legs</a>
+        </li>
+      </router-link>
+      <router-link
+        to="/skills/core"
+        v-slot="{ href, navigate, isActive }"
+        custom
+      >
+        <li :class="[isActive && 'is-active']">
+          <a :href="href" @click="navigate">Core</a>
+        </li>
+      </router-link>
+    </ul>
+  </div>
+</template>
---
+++ src/components/SkillDetailsContainer.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="px-5 mb-5">
+    <h2 class="title is-4">Stats</h2>
+    <div class="columns">
+      <div class="column">
+        <skill-details
+          category="Push"
+          skill="push-up"
+          :points="user.push.totalPoints"
+        ></skill-details>
+        <h4 class="title is-6 mt-5 mb-1">Fatigue</h4>
+        <progress
+          class="progress"
+          :class="fatigueClass(user.push.fatigue)"
+          :value="user.push.fatigue"
+          :max="user.push.maxFatigue"
+        >
+          90%
+        </progress>
+      </div>
+      <div class="column">
+        <skill-details
+          category="Pull"
+          skill="pull-up"
+          points="10"
+        ></skill-details>
+        <h4 class="title is-6 mt-5 mb-1">Fatigue</h4>
+        <progress
+          class="progress"
+          :class="fatigueClass(42)"
+          value="42"
+          max="100"
+        >
+          90%
+        </progress>
+      </div>
+      <div class="column">
+        <skill-details
+          category="legs"
+          skill="squat"
+          points="10"
+        ></skill-details>
+        <h4 class="title is-6 mt-5 mb-1">Fatigue</h4>
+        <progress
+          class="progress"
+          :class="fatigueClass(90)"
+          value="90"
+          max="100"
+        >
+          90%
+        </progress>
+      </div>
+      <div class="column">
+        <skill-details
+          category="core"
+          skill="plank"
+          points="10"
+        ></skill-details>
+        <h4 class="title is-6 mt-5 mb-1">Fatigue</h4>
+        <progress
+          class="progress"
+          :class="fatigueClass(100)"
+          value="100"
+          max="100"
+        >
+          90%
+        </progress>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import skillDetails from "./SkillDetails.vue";
+
+export default {
+  props: ["user"],
+  components: {
+    skillDetails,
+  },
+  methods: {
+    fatigueClass(val) {
+      switch (true) {
+        case val <= 50:
+          return { "is-success": true };
+        case val <= 75:
+          return { "is-warning": true };
+        case val < 100:
+          return "is-danger";
+        default:
+          return "";
+      }
+    },
+  }
+};
+</script>
