b433633d316ab3e4a6f87323518d19654421b5d6
AdamSlater <adam.slater.1010@gmail.com>
2021-10-19 11:23:54 -0700

    list of skills are now dynamically set

--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -3,69 +3,27 @@
     <p class="mb-2 is-italic has-text-centered">
       Click on a button to perform an exercise.
     </p>
-    <div class="vertical-button-group" v-if="$route.params.skill == 'push'">
-      <skill-button
-        category="push"
-        index="0"
-        :level="skillClass(skill.difficulty)"
-        :name="skill.name"
-        :value="calcSkillValue(index)"
-        :cost="calcSkillCost(index)"
-        @increment-skill="
-          incrementSkill('push', calcSkillValue(index), calcSkillCost(index))
-        "
-        v-for="(skill, index) in pushSkills"
-        :key="index"
-        v-show="checkSkillAccess(index, 'push')"
-      ></skill-button>
-    </div>
-    <div class="vertical-button-group" v-if="$route.params.skill == 'pull'">
-      <skill-button
-        category="pull"
-        index="0"
-        :level="skillClass(skill.difficulty)"
-        :name="skill.name"
-        :value="calcSkillValue(index)"
-        :cost="calcSkillCost(index)"
-        @increment-skill="
-          incrementSkill('pull', calcSkillValue(index), calcSkillCost(index))
-        "
-        v-for="(skill, index) in pullSkills"
-        :key="index"
-        v-show="checkSkillAccess(index, 'pull')"
-      ></skill-button>
-    </div>
-    <div class="vertical-button-group" v-if="$route.params.skill == 'legs'">
-      <skill-button
-        category="legs"
-        index="0"
-        :level="skillClass(skill.difficulty)"
-        :name="skill.name"
-        :value="calcSkillValue(index)"
-        :cost="calcSkillCost(index)"
-        @increment-skill="
-          incrementSkill('legs', calcSkillValue(index), calcSkillCost(index))
-        "
-        v-for="(skill, index) in legsSkills"
-        :key="index"
-        v-show="checkSkillAccess(index, 'legs')"
-      ></skill-button>
-    </div>
-    <div class="vertical-button-group" v-if="$route.params.skill == 'core'">
-      <skill-button
-        category="core"
-        index="0"
-        :level="skillClass(skill.difficulty)"
-        :name="skill.name"
-        :value="calcSkillValue(index)"
-        :cost="calcSkillCost(index)"
-        @increment-skill="
-          incrementSkill('core', calcSkillValue(index), calcSkillCost(index))
-        "
-        v-for="(skill, index) in coreSkills"
-        :key="index"
-        v-show="checkSkillAccess(index, 'core')"
-      ></skill-button>
+    <div
+      class="vertical-button-group"
+      v-for="(category, index) in Object.keys(this.skills)"
+      :key="index"
+    >
+      <div v-if="$route.params.skill == category">
+        <skill-button
+          :category="category"
+          index="0"
+          :level="skillClass(skill.difficulty)"
+          :name="skill.name"
+          :value="calcSkillValue(i)"
+          :cost="calcSkillCost(i)"
+          @increment-skill="
+            incrementSkill(category, calcSkillValue(i), calcSkillCost(i))
+          "
+          v-for="(skill, i) in skills[category]"
+          :key="`skill-${i}`"
+          v-show="checkSkillAccess(i, category)"
+        ></skill-button>
+      </div>
     </div>
   </div>
 </template>
@@ -80,10 +38,7 @@ export default {
   data() {
     return {
       user: {},
-      pushSkills: [],
-      pullSkills: [],
-      legsSkills: [],
-      coreSkills: [],
+      skills: {},
     };
   },
   methods: {
@@ -148,10 +103,13 @@ export default {
   },
   mounted() {
     this.user = this.$store.getters.user;
-    this.pushSkills = this.$store.getters.push;
-    this.pullSkills = this.$store.getters.pull;
-    this.legsSkills = this.$store.getters.legs;
-    this.coreSkills = this.$store.getters.core;
+
+    let skills = this.user.skills;
+    skills = skills.map((skill) => skill.category);
+
+    skills.forEach((skill) => {
+      this.skills[skill] = this.$store.getters[skill];
+    });
   },
 };
 </script>
